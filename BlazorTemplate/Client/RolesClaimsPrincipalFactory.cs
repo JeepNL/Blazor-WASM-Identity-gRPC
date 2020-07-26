7fa53fb695a6df6735f32f736e9abf89a84837be
Jaap <github@ictz.net>
2020-07-26 12:48:50 +0200

    IMPORTANT [Bug]: Added: .ToArray() at line 22
    Line 22: `var roleClaims = identity.FindAll(identity.RoleClaimType);` => `var roleClaims = identity.FindAll(identity.RoleClaimType).ToArray();`

--- BlazorTemplate/Client/RolesClaimsPrincipalFactory.cs
+++ BlazorTemplate/Client/RolesClaimsPrincipalFactory.cs
@@ -19,7 +19,7 @@ namespace BlazorTemplate.Client
             if (user.Identity.IsAuthenticated)
             {
                 var identity = (ClaimsIdentity)user.Identity;
-                var roleClaims = identity.FindAll(identity.RoleClaimType);
+                var roleClaims = identity.FindAll(identity.RoleClaimType).ToArray();
                 if (roleClaims != null && roleClaims.Any())
                 {
                     foreach (var existingClaim in roleClaims)
